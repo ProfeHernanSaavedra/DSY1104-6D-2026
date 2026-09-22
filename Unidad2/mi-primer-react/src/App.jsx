8ef8c08be71245fccd0e2d1e9728584a67ea25f6
VI-Docente <vi-docente@duoc.acad>
2026-09-22 18:13:29 -0300

    revisando que es jsx con variables

--- Unidad2/mi-primer-react/src/App.jsx
+++ Unidad2/mi-primer-react/src/App.jsx
@@ -1,10 +1,13 @@
 const nombre = "Hernán"
+const edad = 49
 
 function App(){
   return(
     <div>
       <h1>Hola {nombre}</h1>
       <p>Bienvenido a React</p>
+      <p>Edad: {edad}</p>
+      <p>El próximo año tendras {edad + 1}</p>
     </div>
   )
 }
